1e9cb04a135f10ca698e8fcc637f582b3c8f42c5
jota <jotacaparros@gmail.com>
2021-04-09 20:12:12 +0200

    09/04/21

---
+++ Carrito/scripts/carro.js
@@ -0,0 +1,22 @@
+/*
+ Cambia a gris (#cecece) el color de fondo (background-color) de los artículos (items). Debes utilizar el método getElementsByClassName.
+ Pon un borde (border) sólido, de color negro y 4 píxeles de grosor (4px solid black) al elemento con id cart_items. Debes utilizar el método getElementById.
+ Pon un borde (border) sólido, de color azul y 1 píxel de grosor (1px solid blue) a todas las imágenes de la página. Debes utilizar el método getElementsByTagName.
+ Subraya (text-decoration:underline) los <label> que sean hijos directos de un elemento de la clase item.
+ Pon la fuente de color rojo (color:red) a todos los botones (<button>) que estén dentro del cart_container.
+ Pon la fuente de color blanco (color:white) a todos los <label> que estén adyacentes a otro <label> y estén dentro de un elemento de la clase item.
+ Pon el fondo de color amarillo a todos los <div> que estén vacíos.
+ Pon el fondo de color rojo al primer y último elemento de la clase item.
+ Pon el borde de color verde (border-color:green) a las imágenes de camisetas.
+ Pon la fuente de color verde (color:green) a todos los <input> de la página
+ COMPLICADO: Pon la fuente de color verde (color:green) a todos los elementos que contengan en el texto el símbolo „€‟
+*/
+
+(function cambiarEstilos(){
+    
+    var idSeleccionado = doc 
+    var elementoSeleccionado = document.getElementsByClassName('item');
+    item.style.backgroundColor = '#cecece';
+
+})();
+
